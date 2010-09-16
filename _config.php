f5e61fe248054dd4837aefbec07b3d4c04b4d2e8
Normann Lou <normann@silverstripe.com>
2010-09-16 04:50:31 +0000

    APICHANGE: Rename DPSHarness class to Harness and move it out from DPSPayment, use it as a Harness general interface

--- _config.php
+++ _config.php
@@ -3,7 +3,7 @@
 Director::addRules(50, array(
 	WorldpayPayment_Handler::$URLSegment . '/$Action/$ID' => 'WorldpayPayment_Handler',
 	PayPalPayment_Handler::$URLSegment . '/$Action/$ID' => 'PayPalPayment_Handler',
-	'harness/$Action/$Class/$ID' => 'DPSHarness', 
+	'harness/$Action/$Class/$ID' => 'Harness', 
 ));
 
 Object::add_extension('Member', 'PayerHavingReceipt');
