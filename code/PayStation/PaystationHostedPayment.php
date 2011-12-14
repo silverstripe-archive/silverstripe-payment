83e3e1c768bce549683345c6ce82806e0d966cb0
Frank Mullenger <frankmullenger@gmail.com>
2011-12-14 16:11:35 +1300

    Turn on verification of secure certs.

--- code/PayStation/PaystationHostedPayment.php
+++ code/PayStation/PaystationHostedPayment.php
@@ -268,10 +268,7 @@ class PaystationHostedPayment extends Payment {
   	curl_setopt($ch, CURLOPT_POST,1);
   	curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
   	curl_setopt($ch, CURLOPT_URL, $url);
-  	
-  	curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0); //2
-  	curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0);
-  	
+  	curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 2);
   	curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
   	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   	$result = curl_exec($ch);
