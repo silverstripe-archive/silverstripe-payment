b87c9ea83b897c4692bc739a476ec5807369e386
Ryan Dao <daoduyducduong@gmail.com>
2012-06-19 16:11:26 +0800

    Put all Dummy classes into Dummy.php

--- code/Dummy.php
+++ code/Dummy.php
@@ -37,3 +37,32 @@ class Dummy_ExternalGateway_Controller extends Controller{
     Director::redirect(Session::get('returnurl')); 
   }
 } 
+
+class Dummy_Gateway extends Payment_Gateway {
+
+  public function process($data) {
+    Session::set('amount', $data['Amount']);
+    Director::redirect($this->returnURL);
+  }
+
+  public function getResponse($response) {
+    $amount = Session::get('amount');
+    $cents = round($amount - intval($amount), 2);
+    switch ($cents) {
+      case 0.01:
+        return new Gateway_Result(Gateway_Result::SUCCESS);
+        break;
+      case 0.02:
+        return new Gateway_Result(Gateway_Result::FAILURE);
+        break;
+      case 0.03:
+        return new Gateway_Result(Gateway_Result::INCOMPLETE);
+        break;
+      default:
+        return new Gateway_Result();
+        break;
+    }
+  }
+}
+
+class Dummy_Production_Gateway extends Dummy_Gateway { }
