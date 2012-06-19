ceef1804c203947357004b0d0f9a96371857c842
Ryan Dao <daoduyducduong@gmail.com>
2012-06-20 03:23:46 +0800

    Add skeleton PayPal classes

--- code/PayPal/code/PayPal.php
+++ code/PayPal/code/PayPal.php
@@ -1,1 +1,39 @@
 <?php
+
+class PayPal_Payment extends Payment {
+  
+}
+
+class PayPal_Gateway extends Payment_Gateway {
+  
+  protected $gatewayURL;
+  
+  /**
+   * The PayPal method in use. 
+   * 
+   * @var String
+   */
+  protected $method;
+  
+  public function process($data) {
+    
+  }
+  
+  /**
+   * Tell PayPal to proceed an action
+   * 
+   * @param String $operation
+   */
+  public function operate($action) {
+    
+  }
+}
+
+class PayPal_Gateway_Direct extends PayPal_Gateway {
+  
+}
+
+class PayPal_Gateway_Express extends PayPal_Gateway {
+  
+}
+
