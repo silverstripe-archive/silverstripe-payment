d85da1abc972483cff1a5ba2d3e3b3b0f608f7e7
Ryan Dao <daoduyducduong@gmail.com>
2012-06-24 17:54:13 +0800

    Add code for process() for PayPalDirect and PayPalExpress (untested)

--- code/PayPal/code/PayPal.php
+++ code/PayPal/code/PayPal.php
@@ -24,6 +24,13 @@ class PayPal_Gateway extends Payment_Gateway {
   protected $paypalMethod;
   
   /**
+   * The data to be posted to PayPal server
+   * 
+   * @var array
+   */
+  protected $postData;
+  
+  /**
    * Get the PayPal configuration 
    */
   public static function get_config() {
@@ -64,29 +71,39 @@ class PayPal_Gateway extends Payment_Gateway {
   }
   
   public function process($data) {
+    $authentication = self::get_authentication();
+    
     // Prepare the payment data in PayPal's format
     // TODO: Check if credit card data and return url is set
-    $postData = array(
-      'PAYMENTACTION' => self::$action,
-      'AMT' => $data['Amount'],
-      'CURRENCY' => $data['Currency'],
-      'IP' => $_SERVER['REMOTE_ADDR'],
-      'USER' => self::$user,
-      'PWD' => self::$password,
-      'SIGNATURE' => self::$signature
-    ); 
-    
-    // Post the data to PayPal server
-    $response = $this->postPaymentData($postData);
-    
-    // TODO: if return url is set for ExpressCheckout, no need to do manual redirection
-    Session::set('PayPalResponse', $response);
-    Director::redirect($this->returnURL);
+    $this->postData['PAYMENTACTION'] = self::$action;
+    $this->postData['AMT'] = $data['Amount'];
+    $this->postData['CURRENCY'] = $data['Currency'];
+    $this->postData['IP'] = $_SERVER['REMOTE_ADDR'];
+    $this->postData['USER'] = $authentication['user'];
+    $this->postData['PWD'] = $authentication['password'];
+    $this->postData['SIGNATURE'] = $authentication['signature'];
   }
 }
 
 class PayPalDirect_Gateway extends PayPal_Gateway {
   protected $paypalMethod = 'DoDirectPayment';
+  
+  public function process($data) {
+    parent::process($data);
+    
+    // Add credit card data. May have to parse the data to fit PayPal's format
+    $this->postData['ACCT'] = $data['CardNumber'];
+    $this->postData['EXPDATE'] = $data['DateExpiry'];
+    $this->postData['CVV2'] = $data['Cvc2'];
+    // And billing information as well
+    
+    // Post the data to PayPal server
+    return $this->postPaymentData($this->postData);
+  }
+  
+  public function getResponse($response) {
+    
+  }
 }
 
 class PayPalDirect_Gateway_Production extends PayPalDirect_Gateway { }
@@ -95,6 +112,20 @@ class PayPalDirect_Gateway_Dev extends PayPalDirect_Gateway { }
 
 class PayPalExpress_Gateway extends PayPal_Gateway {
   protected $paypalMethod = 'SetExpressCheckout';
+  
+  public function process($data) {
+    parent::process($data);
+  
+    // Add return url
+    $this->postData['RETURNURL'] = $this->returnURL;
+    
+    // Post the data to PayPal server
+    return $this->postPaymentData($this->postData);
+  }
+  
+  public function getResponse($response) {
+    
+  }
 }
 
 class PayPalExpress_Gateway_Production extends PayPalExpress_Gateway { }
