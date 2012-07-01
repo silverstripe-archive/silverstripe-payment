490df7159696b46cc00b468a40960e06d8a21478
Ryan Dao <daoduyducduong@gmail.com>
2012-07-01 15:27:04 +0800

    Add DummyMerchantHosted white-box testing

--- code/Dummy.php
+++ code/Dummy.php
@@ -67,3 +67,56 @@ class DummyGatewayHosted_Gateway extends Payment_Gateway {
     }
   }
 }
+
+/**
+ * Imaginary place to visit external gateway
+ */
+class DummyExternalGateway_Controller extends Controller{
+
+  function pay($request) {
+    return $this->customise(array(
+        'Content' => "<h1>Fill out this form to make payment</h1>",
+        'Form' => $this->PayForm()
+    ))->renderWith('Page');
+  }
+
+  function PayForm() {
+    $fields = new FieldList(
+        new TextField('CardHolderName', 'Card Holder Name'),
+        new CreditCardField('CardNumber', 'Card Number'),
+        new DateField('DateExpiry', 'Expiration date')
+    );
+
+    $actions = new FieldList(
+        new FormAction("dopay")
+    );
+
+    return new Form($this, "PayForm", $fields, $actions);
+  }
+
+  function dopay() {
+    if ($data = Session::get('paymentData')) {
+      $amount = $data['Amount'];
+      $cents = round($amount - intval($amount), 2);
+
+      switch ($cents) {
+        case 0.00:
+          Session::set('result', Payment_Gateway_Result::SUCCESS);
+          break;
+        case 0.01:
+          Session::set('result', Payment_Gateway_Result::FAILURE);
+          break;
+        case 0.02:
+          Session::set('result', Payment_Gateway_Result::INCOMPLETE);
+          break;
+        default:
+          Session::set('result', Payment_Gateway_Result::FAILURE);
+          break;
+      }
+
+      Controller::redirect($data['ReturnURL']);
+    } else {
+      user_error("No user data is set for this transaction", E_USER_ERROR);
+    }
+  }
+}
---
+++ tests/Dummy/DummyTest.php
@@ -0,0 +1,55 @@
+<?php
+
+class DummyMerchantHostedTest extends SapphireTest {
+  
+  /* Black-box testing */
+  function testClassConfig() {
+    $controller = Payment_Controller::factory('DummyMerchantHosted');
+    $this->assertEquals(get_class($controller), 'Payment_Controller_MerchantHosted');
+    $this->assertEquals(get_class($controller->gateway), 'DummyMerchantHosted_Gateway');
+    $this->assertEquals(get_class($controller->payment), 'Payment');
+  }
+  
+  function testProcessPayment() {
+    $successData = array(
+      'Amount' => '10',
+      'Currency' => 'USD'   
+    );
+    
+    $failureData = array(
+      'Amount' => '10.01',
+      'Currency' => 'USD'  
+    );
+    
+    $incompleteData = array(
+      'Amount' => '10.02',
+      'Currency' => 'USD'  
+    );
+    
+    $controller = Payment_Controller::factory('DummyMerchantHosted');
+    $successResult = $controller->processPayment($successData);
+    $failureResult = $controller->processPayment($failureData);
+    $incompleteResult = $controller->processPayment($incompleteData);
+    
+    $this->assertEquals($successResult, new Payment_Gateway_Result(Payment_Gateway_Result::SUCCESS));
+    $this->assertEquals($failureResult, new Payment_Gateway_Result(Payment_Gateway_Result::FAILURE));
+    $this->assertEquals($incompleteResult, new Payment_Gateway_Result(Payment_Gateway_Result::INCOMPLETE));
+    
+    // TODO: Assert correct model
+  }  
+}
+
+class DummyGatewayHostedTest extends FunctionalTest {
+  
+  /* Black-box testing */
+  function testClassConfig() {
+    $controller = Payment_Controller::factory('DummyGatewayHosted');
+    $this->assertEquals(get_class($controller), 'Payment_Controller_GatewayHosted');
+    $this->assertEquals(get_class($controller->gateway), 'DummyGatewayHosted_Gateway');
+    $this->assertEquals(get_class($controller->payment), 'Payment');
+  }
+  
+  function testPaymentFlow() {
+    
+  }
+}
