da533cc3ec4c61b2fff7bcfcd528ec95cdfe98bc
Ryan Dao <daoduyducduong@gmail.com>
2012-08-18 18:27:44 -0700

    Change 'controller' to 'processor' in PaymentFactory

--- code/PaymentFactory.php
+++ code/PaymentFactory.php
@@ -17,17 +17,17 @@ class PaymentFactory {
       return null;
     }
   }
-  
+
   /**
    * Get the gateway object that will be used for the given payment method.
    * The gateway class is automatically retrieved based on configuration
-   * 
+   *
    * @param String $methodName
    */
   static function get_gateway($methodName) {
     // Get the gateway environment setting
     $environment = PaymentGateway::get_environment();
-  
+
     // Get the custom class configuration if applicable.
     // If not, apply naming convention.
     $methodConfig = self::get_factory_config($methodName);
@@ -47,20 +47,20 @@ class PaymentFactory {
           break;
       }
     }
-  
+
     if (class_exists($gatewayClass)) {
       return new $gatewayClass();
     } else {
       throw new Exception("$gatewayClass class does not exists.");
     }
   }
-  
+
   /**
    * Get the payment object that will be used for the given payment method.
-   * 
-   * The payment class is automatically retrieved based on naming convention 
-   * if not specified in the yaml config. 
-   * 
+   *
+   * The payment class is automatically retrieved based on naming convention
+   * if not specified in the yaml config.
+   *
    * @param String methodName
    */
   static function get_payment_model($methodName) {
@@ -72,40 +72,40 @@ class PaymentFactory {
     } else {
       $paymentClass = 'Payment';
     }
-  
+
     if (class_exists($paymentClass)) {
       return new $paymentClass();
     } else {
       throw new Exception("$paymentClass class does not exists");
     }
   }
-  
+
   /**
-   * Factory function to create payment controller object
+   * Factory function to create payment processor object
    *
    * @param String $methodName
    * @return PaymentProcessor
    */
   public static function factory($methodName) {
     $supported_methods = PaymentProcessor::get_supported_methods();
-  
+
     if (! in_array($methodName, $supported_methods)) {
       throw new Exception("The method $methodName is not supported");
     }
-  
+
     $methodConfig = self::get_factory_config($methodName);
-    if (isset($methodConfig['controller'])) {
-      $controllerClass = $methodConfig['controller'];
-      $controller = new $controllerClass();
-      $controller->setMethodName($methodName);
-  
+    if (isset($methodConfig['processor'])) {
+      $processorClass = $methodConfig['processor'];
+      $processor = new $processorClass();
+      $processor->setMethodName($methodName);
+
       // Set the dependencies
-      $controller->gateway = self::get_gateway($methodName);
-      $controller->payment = self::get_payment_model($methodName);
-  
-      return $controller;
+      $processor->gateway = self::get_gateway($methodName);
+      $processor->payment = self::get_payment_model($methodName);
+
+      return $processor;
     } else {
-      throw new Exception("No controller is defined for the method $methodName");
+      throw new Exception("No processor is defined for the method $methodName");
     }
   }
 }
