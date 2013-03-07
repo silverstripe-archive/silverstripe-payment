7bdf170abc00dde8185e40fa8ad423c2a1f76703
Russell Michell <russell@silverstripe.com>
2013-03-07 13:20:32 +1300

    MINOR: Changed default priority to 100 for Director::addRule() as per 3.0 deprecation message

--- _config.php
+++ _config.php
@@ -1,6 +1,6 @@
 <?php
 
-Director::addRules(50, array(
+Director::addRules(100, array(
 	WorldpayPayment_Handler::$URLSegment . '/$Action/$ID' => 'WorldpayPayment_Handler',
 	PayPalPayment_Handler::$URLSegment . '/$Action/$ID' => 'PayPalPayment_Handler',
 ));
